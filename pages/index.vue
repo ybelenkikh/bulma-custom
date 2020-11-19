b8f6c0665abae0763604492b1c03b72700e8dd1e
ybelenkikh <ybelenkikh@ya.ru>
2020-11-19 15:00:04 +0300

    'start-2'

---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+<div>
+ <div>
+  <h4>BUTTON</h4>
+  <button class="button">button.button</button>
+  <button class="button is-primary is-small">button.button.is-primary.is-small</button>
+  <button class="button is-link is-medium">button.button.is-link.is-medium</button>
+  <button class="button is-info is-large">button.button.is-info.is-large</button>
+  <button class="button is-success">button.button.is-success</button>
+  <button class="button is-warning">button.button.is-warning</button>
+  <button class="button is-danger">button.button.is-danger</button>
+  <button class="button is-danger is-outlined">button.button.is-danger.is-outlined</button>
+  <button class="button is-loading">button.button.is-loading</button>
+  <button class="button" disabled>button.button disabled</button>
+ </div>
+ <hr>
+ <div class="columns">
+ <div class="column">
+   <h4>MARGIN</h4>
+   <p>mt-*</p>
+   <p>mr-*</p>
+   <p>mb-*</p>
+   <p>ml-*</p>
+ </div>
+ <div class="column">
+   <h4>PADDING</h4>
+   <p>pt-*</p>
+   <p>pr-*</p>
+   <p>pb-*</p>
+   <p>pl-*</p>
+ </div>
+ <div class="column">
+   <h4>SIZE</h4>
+   <p>*-0 0</p>
+   <p>*-1 0.25rem</p>
+   <p>*-2 0.5rem</p>
+   <p>*-3 0.75rem</p>
+   <p>*-4 1rem</p>
+   <p>*-5 1.5rem</p>
+   <p>*-6 3rem</p>
+ </div>
+ </div>
+ <hr>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+
+</style>
